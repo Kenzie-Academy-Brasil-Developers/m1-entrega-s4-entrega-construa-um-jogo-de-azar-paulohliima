a47e1bc604c422b70972993917e3acfcb19db0ad
paulohliima <paulo.lima-16@hotmail.com>
2021-11-25 19:58:15 -0300

    Ajeitando JS Hunting Words

--- assets/js/hunting-words.js
+++ assets/js/hunting-words.js
@@ -6,8 +6,11 @@ const dictionary = ["BANANA","COMIDA","ABACATE",
 const divPuzzle = document.getElementsByClassName("game--board")[0]
 const table = document.createElement("table")
 const tbody = document.createElement("tbody")
-//END global-variables
 
+
+
+
+//END global-variables
 //START dynamic-content-main
 
 const verifyWord = (i) =>{
@@ -58,125 +61,106 @@ const tableWords = () =>{
             colunm.appendChild(cell)
 
         }
-        correctlyWord(wordsFound.join(""));
     }
+    table.appendChild(tbody)
+    divPuzzle.appendChild(table)
+    console.table(arrTable);
+    positionTable(arrTable);
+}
 
-    // const correctlyWord = (letterCorrectly) =>{
-    //     const checkWord = document.getElementsByClassName("game--words")[0]
-    //     const columnWord1 = document.getElementsByClassName("words--column--01")[0]
-    //     const columnWord2 = document.getElementsByClassName("words--column--02")[0]
+const selectWords = () =>{
+    let arrWordsSelect = []
+    let wordRandom;
+    let nameRandom;
+    for(let i = 0; i < 3; i++){
+        wordRandom = Math.floor(Math.random() * 19);
 
-    //     for()
-
-    //     console.log(columnWord1.innerHTML);
-    //     console.log(letterCorrectly);
-        
-    // }
-    const tableWords = () =>{
-        let arrTable = []
-    
-        for(let i = 0; i < 10; i++){
-            arrTable[i] = []
-
-            const colunm = document.createElement("tr")
-            tbody.appendChild(colunm)
-
-            for(let j = 0; j < 10; j++){
-                arrTable[i][j] = [];
-
-                const cell = document.createElement("td")
-                colunm.appendChild(cell)
-
-            }
-        }
-        table.appendChild(tbody)
-        divPuzzle.appendChild(table)
-        positionTable(arrTable);
-    }
-
-    const selectWords = () =>{
-        let arrWordsSelect = []
-        let wordRandom;
-        let nameRandom;
-        for(let i = 0; i < 3; i++){
+        if(dictionary[wordRandom] === nameRandom || i === 2 && dictionary[wordRandom] === arrWordsSelect[0]){
             wordRandom = Math.floor(Math.random() * 19);
-
-            if(dictionary[wordRandom] === nameRandom || i === 2 && dictionary[wordRandom] === arrWordsSelect[0]){
-                wordRandom = Math.floor(Math.random() * 19);
-                i--;
-            }else{
-                nameRandom = dictionary[wordRandom]
-                arrWordsSelect.push(nameRandom);
-            }
+            i--;
+        }else{
+            nameRandom = dictionary[wordRandom]
+            arrWordsSelect.push(nameRandom);
         }
-        return arrWordsSelect;
     }
+    return arrWordsSelect;
+}
 
-    const positionTable = (wordsTable) =>{
-        const elementWord = document.getElementsByTagName("tbody")[0];
-        const wordsSelect = selectWords();
-        let elementWordTd;
+const positionTable = (wordsTable) =>{
+    const elementWord = document.getElementsByTagName("tbody")[0];
+    const wordsSelect = selectWords();
+    let elementWordTd;
 
 
 
-        let lineP;
-        let columnP;
-        let currentWord;
-        let currentLine = [];
-        let countWord = 0;
-        
-        for(let i = 0; i < wordsSelect.length; i++){ // roda 3 vezes por conta do tamanho do array de palavras selecionadas
-            lineP = Math.floor(Math.random() * 9);
-            columnP = Math.floor(Math.random() * 9);
-            countWord = 0;
-            if(wordsSelect[i].length < wordsTable[lineP].length - columnP+1 && currentWord !== wordsSelect[i] && !currentLine.includes(wordsTable[lineP])){ //se o tamanho de cada palavra for menor ou igual ao 
-                currentWord = wordsSelect[i];
-                currentLine.push(wordsTable[lineP]);
-                
-                for(let l = 0; l < wordsSelect[i].length; l++){ 
-                    wordsTable[lineP][columnP+countWord] = wordsSelect[i][l];
-
-                    elementWordTd = elementWord.rows[lineP]
-                    elementWordTd.childNodes[columnP+countWord].innerText = wordsSelect[i][l];
-                    elementWordTd.childNodes[columnP+countWord].classList.add(`letter-${i}`)
-                    elementWordTd.childNodes[columnP+countWord].addEventListener("click", () => verifyWord(i));
-
-                    countWord += 1;
-                }
-            }else{
-                i--;
-            }
+    let lineP;
+    let columnP;
+    let currentWord;
+    let currentLine = [];
+    let countWord = 0;
+    
+    for(let i = 0; i < wordsSelect.length; i++){ // roda 3 vezes por conta do tamanho do array de palavras selecionadas
+        lineP = Math.floor(Math.random() * 9);
+        columnP = Math.floor(Math.random() * 9);
+        countWord = 0;
+        if(wordsSelect[i].length < wordsTable[lineP].length - columnP+1 && currentWord !== wordsSelect[i] && !currentLine.includes(wordsTable[lineP])){ //se o tamanho de cada palavra for menor ou igual ao 
+            currentWord = wordsSelect[i];
+            currentLine.push(wordsTable[lineP]);
             
+            for(let l = 0; l < wordsSelect[i].length; l++){ 
+                wordsTable[lineP][columnP+countWord] = wordsSelect[i][l];
+
+                elementWordTd = elementWord.rows[lineP]
+                elementWordTd.childNodes[columnP+countWord].innerText = wordsSelect[i][l];
+                elementWordTd.childNodes[columnP+countWord].classList.add(`letter-${i}`)
+                elementWordTd.childNodes[columnP+countWord].addEventListener("click", () => verifyWord(i));
+
+                countWord += 1;
+            }
+        }else{
+            i--;
+            console.log("não cabe");
         }
-        randomLetters(wordsTable);
+        
     }
+    console.table(wordsTable)
+    randomLetters(wordsTable);
+}
 
-    const randomLetters = (tableWords) =>{
-        const elementWord = document.getElementsByTagName("tbody")[0];
-        const wordsSelect = selectWords();
-        let elementWordTd;
+const randomLetters = (tableWords) =>{
+    const elementWord = document.getElementsByTagName("tbody")[0];
+    const wordsSelect = selectWords();
+    let elementWordTd;
 
 
-        const aToZ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
-        let randomP;
-        for(let i = 0; i < tableWords.length; i++){
-            for(let j = 0; j < tableWords[i].length; j++){
-                randomP = Math.floor(Math.random() * 25);
-                if(tableWords[i][j].length === 0){
-                    tableWords[i][j] = aToZ[randomP]
+    const aToZ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+    let randomP;
+    for(let i = 0; i < tableWords.length; i++){
+        for(let j = 0; j < tableWords[i].length; j++){
+            randomP = Math.floor(Math.random() * 25);
+            if(tableWords[i][j].length === 0){
+                tableWords[i][j] = aToZ[randomP]
 
-                    elementWordTd = elementWord.rows[i]
-                    elementWordTd.childNodes[j].innerText = aToZ[randomP];
-                }
+                elementWordTd = elementWord.rows[i]
+                elementWordTd.childNodes[j].innerText = aToZ[randomP];
             }
         }
     }
-//END dynamic-content-main
+    console.table(tableWords);
+}
 
+//END dynamic-content-main
 //START call-functions
+
 tableWords();
 selectWords();
+
+
 //END call-functions
 
 //START event-listeners
+
+
+
 //END event-listeners
+
