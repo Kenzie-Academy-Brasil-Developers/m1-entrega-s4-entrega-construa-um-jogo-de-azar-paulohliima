e2e9bd88e655da2e3e01c542dd847624eba3b209
Leonardo Moraes <leogm360@gmail.com>
2021-11-25 08:38:18 -0300

    Refatorar Código
    Refatorar código dos arquivos index.html, style.css, switch-button.js e
    slot-macine.js;
    
    Modificar formato dos efeitos sonoros para mpeg e incluir cópia em ogg
    para efeitos de compatibilidade;
    
    Modificar estrutura dos repositórios de imagem.

--- assets/js/slot-machine.js
+++ assets/js/slot-machine.js
@@ -1,71 +1,80 @@
 //START global-variables
-const buttonGame02 = document.getElementById("slot-roll");
-const imagem = document.getElementsByClassName("game--background--black")[0];
-const img1 = document.getElementsByClassName("div--box--img")[0];
-const img2 = document.getElementsByClassName("div--box--img")[1];
-const img3 = document.getElementsByClassName("div--box--img")[2];
-
+    const buttonGame02 = document.getElementById("slot-roll");
+    const leverRod = document.getElementById("slot-rod");
+    const imagem = document.getElementsByClassName("game--background--black")[0];
+    const img1 = document.getElementsByClassName("slot--image")[0];
+    const img2 = document.getElementsByClassName("slot--image")[1];
+    const img3 = document.getElementsByClassName("slot--image")[2];
 //END global-variables
 
-//START dynamic-content-header
-//END dynamic-content-header
-
 //START dynamic-content-main
- 
-const Aleatorio = () =>{
-    return Math.floor(Math.random() * 12) +1;
-}
-
-const conditionWin = (img1, img2, img3) =>{
-    if(img1 === img2 && img2 === img3 ){
-        console.log("JackPot")
-    }else{
-        console.log("Tente Novamente!")
+    const Aleatorio = () =>{
+        return Math.floor(Math.random() * 12) +1;
     }
-}
-const nextImg = () =>{
-    let img1Variable;
-    let img2Variable;
-    let img3Variable;
-    img1Variable = Aleatorio();
-    img1.src=`./assets/img/game02/img${img1Variable}.png`;
-    img2Variable = Aleatorio();
-    img2.src=`./assets/img/game02/img${img2Variable}.png`;
-    img3Variable = Aleatorio();
-    img3.src=`./assets/img/game02/img${img3Variable}.png`;
-}
 
-const imgAnimation = () =>{
-    buttonGame02.removeEventListener("click", imgAnimation);
-    let interval = setInterval(() => {
-        nextImg();
-    },100)
-    setTimeout(() => {
-        adding()
-        clearInterval(interval)}, 6000);
-}
+    const conditionWin = (img1, img2, img3) =>{
+        if(img1 === img2 && img2 === img3 ){
+            console.log("JackPot")
+        }else{
+            console.log("Tente Novamente!")
+        }
+    }
+    const nextImg = () =>{
+        let img1Variable;
+        let img2Variable;
+        let img3Variable;
+        img1Variable = Aleatorio();
+        img1.src=`./assets/img/caça-níqueis/img${img1Variable}.png`;
+        img2Variable = Aleatorio();
+        img2.src=`./assets/img/caça-níqueis/img${img2Variable}.png`;
+        img3Variable = Aleatorio();
+        img3.src=`./assets/img/caça-níqueis/img${img3Variable}.png`;
+    }
 
-const adding = (e) =>{
-    // imgAnimation();
-    let img1Variable;
-    let img2Variable;
-    let img3Variable;
+    const imgAnimation = () =>{
+        buttonGame02.removeEventListener("click", imgAnimation);
+        let interval = setInterval(() => {
+            nextImg();
+        },100)
+        setTimeout(() => {
+            adding()
+            clearInterval(interval)}, 6000);
+        
+        //Class-toggle
+        buttonGame02.classList.toggle('animation');
+        leverRod.classList.toggle('animation');
 
-    img1Variable = Aleatorio();
-    img1.src=`./assets/img/game02/img${img1Variable}.png`;
-    img2Variable = Aleatorio();
-    img2.src=`./assets/img/game02/img${img2Variable}.png`;
-    img3Variable = Aleatorio();
-    img3.src=`./assets/img/game02/img${img3Variable}.png`;
+        setTimeout(
+            () => {
+                //Variable
+                const button = document.getElementById("slot-roll");
+                const rod = document.getElementById("slot-rod");
 
-    conditionWin(img1Variable, img2Variable, img3Variable)
-    buttonGame02.addEventListener("click", imgAnimation);
-}
+                //Class-toggle
+                button.classList.toggle('animation');
+                rod.classList.toggle('animation');
+            },
+            2500
+        );
+    }
 
+    const adding = (e) =>{
+        // imgAnimation();
+        let img1Variable;
+        let img2Variable;
+        let img3Variable;
+
+        img1Variable = Aleatorio();
+        img1.src=`./assets/img/caça-níqueis/img${img1Variable}.png`;
+        img2Variable = Aleatorio();
+        img2.src=`../assets/img/caça-níqueis/img${img2Variable}.png`;
+        img3Variable = Aleatorio();
+        img3.src=`./assets/img/caça-níqueis/img${img3Variable}.png`;
+
+        conditionWin(img1Variable, img2Variable, img3Variable)
+        buttonGame02.addEventListener("click", imgAnimation);
+    }
 //END dynamic-content-main
-
-//START call-functions
-//END call-functions
 
 //START event-listeners
 buttonGame02.addEventListener("click", imgAnimation);
--- assets/js/switch-button.js
+++ assets/js/switch-button.js
@@ -1,14 +1,32 @@
+//START global-variables
+    const standbySound = document.getElementById('standby-slot-machine');
+    const wheelRoll = document.getElementById('wheelroll-slot-machine');
+    const winning = document.getElementById('winning-slot-machine');
+//END global-variables
+
 //START dynamic-content-main
-    //END switch-button
+    //START sound-effects
+        //START hunting-words
+            
+        //END hunting-words  
+
+        //START slot-machine
+            function playBackgroundSound(){
+                
+            };
+        //END slot-machine
+    //END sound-effects
+
+    //START switch-button
         function toggleGames(){
             //Variables
             const wordSeeker = document.getElementById('game1');
             const slotMachine = document.getElementById('game2');
 
             //Classes-toggle
-            wordSeeker.classList.toggle('hidden');
+            wordSeeker.classList.toggle('hide');
             wordSeeker.classList.toggle('show');
-            slotMachine.classList.toggle('hidden');
+            slotMachine.classList.toggle('hide');
             slotMachine.classList.toggle('show');
         };
 
@@ -23,7 +41,7 @@
             else{
                 game.innerText = 'Caça-Palavras';
             }
-        }
+        };
 
         function switchGame(event){
             //Variables
@@ -38,12 +56,11 @@
                 toggleGames();
             }
         };
-
     //END switch-button
 //END dynamic-content-main
 
 //START event-listeners
-    const switchButton = document.getElementById('switch-game');
+    const switchButton = document.getElementById('switch-games');
 
     switchButton.addEventListener('click', switchGame);
 //END event-listeners
